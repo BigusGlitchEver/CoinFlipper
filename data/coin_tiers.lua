deba7a4f79a57b30d8d5c7725386ed6d52d54b7a
Shmoo <samuelshmoobigus@yahoo.com>
2026-05-28 22:11:01 +0200

    feat(tiers): export EASY_COIN_RADIUS_SCALE = 0.82

--- data/coin_tiers.lua
+++ data/coin_tiers.lua
@@ -6,11 +6,15 @@
 -- Single source of truth: entities/coin.lua reads .color in draw,
 -- states/game.lua reads .mult in resolveFlip.
 
+local EASY_COIN_RADIUS_SCALE = 0.82  -- easy coins are 82% of standard radius
+
 local Tiers = {
   { color = { 0xF0/255, 0xC0/255, 0x40/255 }, mult = 1.00 },  -- 0 yellow (fresh)
   { color = { 0x44/255, 0x88/255, 0xFF/255 }, mult = 0.75 },  -- 1 blue
   { color = { 0xAA/255, 0x44/255, 0xFF/255 }, mult = 0.50 },  -- 2 purple
   { color = { 0xFF/255, 0x44/255, 0x44/255 }, mult = 0.25 },  -- 3 red
 }
+
+Tiers.EASY_COIN_RADIUS_SCALE = EASY_COIN_RADIUS_SCALE
 
 return Tiers
